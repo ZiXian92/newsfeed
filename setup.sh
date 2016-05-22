8f111b971f0f2b00c8c64c7ccdaf7441f6aaedbe
zixian92 <zixian1992@hotmail.com>
2016-05-22 14:51:50 +0800

    Adds setup script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DB_DIR="db/data"
+ERR_MSG="Please make sure docker and docker-compose are installed"
+
+trap "echo $ERR_MSG" ERR
+
+if [ ! -d "$DB_DIR" ]; then
+  mkdir "$DB_DIR"
+fi
+
+docker-compose up -d
